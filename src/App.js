54884dcde46fe4c85b79234ba630eddbc230ee6e
Jennifer Hail <jen@mightily.com>
2021-01-27 12:32:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+// React
+import React, { useState, useEffect, useRef } from "react";
+// CSS
+import "./app.scss";
+// Data
+import { data } from "./data";
+// Animations
+import { gsap } from "gsap";
 
 function App() {
+  const [items, setItems] = useState(data);
+  const [index, setIndex] = useState(0);
+  // disabling the slider buttons at start and end
+  const [prevDisabled, setPrevDisabled] = useState(false);
+  const [nextDisabled, setNextDisabled] = useState(false);
+
+  let x = 0;
+  // let sliderRef = [];
+  let slider = useRef(null);
+  // let slidesRef = [];
+  let slides = useRef(null);
+
+  useEffect(() => {
+    const lastIndex = items.length - 1;
+    // Disable prev btn at last slide
+    if (index === 0) {
+      setPrevDisabled(true);
+      setNextDisabled(false);
+    }
+    // Enable both btns if slide isn't first or last
+    if (index > 0) {
+      setPrevDisabled(false);
+      setNextDisabled(false);
+    }
+    // Disable next btn at last slide
+    if (index === lastIndex) {
+      setPrevDisabled(false);
+      setNextDisabled(true);
+    }
+  }, [index, items]);
+
+  // Counter animation
+  const moveDots = () => {
+    console.log("moveDots");
+  };
+
+  // Slide movement
+  const moveSlides = (dir) => {
+    setIndex(index + dir);
+    x = (index + dir) * 100 * -1;
+
+    gsap.to(slider, {
+      xPercent: x,
+      duration: 0.6,
+    });
+
+    moveDots();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <div className="slider">
+        <div className="slide-wrapper" ref={(el) => (slider = el)}>
+          {items.map((item, itemIndex) => {
+            const { id, src, alt } = item;
+            let position = "";
+            if (itemIndex === index) {
+              position = " active";
+            }
+            if (itemIndex < index) {
+              position = " visited";
+            }
+            return (
+              <div
+                key={id}
+                className={`slide${position}`}
+                ref={(el) => (slides = el)}
+              >
+                <img src={src} alt={alt} />
+              </div>
+            );
+          })}
+        </div>
+        <div className="counters">
+          {items.map((item, itemIndex) => {
+            let position = "";
+            if (itemIndex === index) {
+              position = " active";
+            }
+            if (itemIndex < index) {
+              position = " visited";
+            }
+            return <div key={item.id} className={`counter${position}`}></div>;
+          })}
+        </div>
+        <div className="controls">
+          <button
+            type="button"
+            className="prev btn"
+            disabled={prevDisabled}
+            onClick={() => moveSlides(-1)}
+          >
+            <img src="./arrow.svg" alt="View previous slide" />
+          </button>
+          <button
+            type="button"
+            className="next btn"
+            disabled={nextDisabled}
+            onClick={() => moveSlides(1)}
+          >
+            <img src="./arrow.svg" alt="View next slide" />
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    id: 1,
+    src: "../slides/img01.png",
+    alt: "Alt text for image one.",
+    caption: "Caption for image one.",
+    active: true,
+  },
+  {
+    id: 2,
+    src: "../slides/img02.png",
+    alt: "Alt text for image two.",
+    caption: "Caption for image two.",
+    active: false,
+  },
+  {
+    id: 3,
+    src: "../slides/img03.png",
+    alt: "Alt text for image three.",
+    caption: "Caption for image three.",
+    active: false,
+  },
+  {
+    id: 4,
+    src: "../slides/img04.png",
+    alt: "Alt text for image four.",
+    caption: "Caption for image four.",
+    active: false,
+  },
+];
+
+// export const query = `
+// {
+//   pageCollection {
+//     items {
+//       title
+//       logo {
+//         url
+//       }
+//       sliderBlockCollection (limit:4) {
+//         items {
+//           image {
+//             url
+//           }
+//           altText
+//           caption
+//         }
+//       }
+//     }
+//   }
+// }
+// `;
